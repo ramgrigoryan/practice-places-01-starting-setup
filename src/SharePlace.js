9fa9ffd3b493e94db54fb9772ac79a4132f6497f
Vahram <ramgrigoryan@gmail.com>
2022-04-09 17:38:55 +0400

    project setup

---
+++ src/SharePlace.js
@@ -0,0 +1,10 @@
+class FindPlace {
+  constructor() {
+    const addressForm = document.querySelector("form");
+    const locateUserBtn = document.getElementById("locate-btn");
+    locateUserBtn.addEventListener("click", this.locateUserHandler);
+    addressForm.addEventListener("submit", this.findAddressHandler);
+  }
+  locateUserHandler() {}
+  findAddressHandler() {}
+}
